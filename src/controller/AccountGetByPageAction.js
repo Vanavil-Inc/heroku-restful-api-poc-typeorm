27202120f7b58ce91d6a1d0de7b40f1ad09ca565
Harxen <harxen.k@gmail.com>
2018-09-26 19:35:21 -0400

    Added a filter criteria for SQL WHERE operator. The filter takes a maximum of 5 criterias. Any more than 5 and it defaults to no filters.

--- src/controller/AccountGetByPageAction.js
+++ src/controller/AccountGetByPageAction.js
@@ -11,8 +11,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
 const Account_1 = require("../entity/Account");
 const User = require("../controller/UserController");
+const validatorColumn = require('node-input-validator');
+var faccount;
 
-var size, skip, pagenumber, pagetotal, size, search;
+var size, skip, pagenumber, pagetotal, fieldlist, filter, tot;
 
 /**
  * Loads account by a given id.
@@ -27,6 +29,21 @@ function accountGetByPageAction(request, response) {
         console.log("User not logged in");
     } else {
         return __awaiter(this, void 0, void 0, function* () {
+          // validator for all fields
+          let validator = new validatorColumn(request.body,{
+            size:'required|integer',
+            page:'required|integer',
+            fieldlist:'required|string',
+            filter:'required|string'
+          });
+          validator.check().then(function (matched) {
+              if(!matched) {
+                response.json({
+                    message:"Please Check To Make Sure Inputs Are Of Proper Type"
+                  })
+                response.end();
+              }
+            })
             // get a account repository to perform operations with account
             const accountRepository = typeorm_1.getManager().getRepository(Account_1.Account);
             // getting account list for help in pagination
@@ -45,25 +62,64 @@ function accountGetByPageAction(request, response) {
               response.end();
               return;
             }
-             //load accounts by given page number by given size
+            // load accounts by given page number by given size
             //  const account = yield accountRepository.createQueryBuilder("accounts").select([
             //    "accounts.id",
             //    "accounts.name"
             // ]).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
-            // Load accounts by body specified parameter
-            search = String(request.body.search).split(" ");
-            var data = [];
-            for (var i = 0; i < search.length; i++) {
-              // data += "\"accounts." + search[i] + "\", ";
-              // data.push("\"accounts\".\"" + search[i] + "\"");
-              data.push(search[i]);
+            // fieldset criteria
+            fieldlist = String(request.body.fieldlist).split(" ");
+            var datafl = [];
+            for (var i = 0; i < fieldlist.length; i++) {
+              datafl.push(fieldlist[i]);
             }
-            console.log(data);
-            console.log(data.length);
-            // data = String(data.slice(0,-2));
-            // console.log(data.length);
-            // console.log(data);
-            const account = yield accountRepository.createQueryBuilder("accounts").select(data).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+            // filter criteria
+            filter = String(request.body.filter).split(", ");
+            var datafr = [];
+            if ((filter.length >= 1) && filter.length <= 5) {
+              for (var i = 0; i < filter.length; i++) {
+                datafr.push(filter[i]);
+              }
+              switch(filter.length) {
+                case 1:
+                  faccount = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+                  tot = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).orderBy("accounts.id", "ASC").skip(skip).getMany();
+                break;
+
+                case 2:
+                  faccount = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+                  tot = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).orderBy("accounts.id", "ASC").skip(skip).getMany();
+                break;
+
+                case 3:
+                  faccount = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).andWhere(datafr[2]).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+                  tot = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).andWhere(datafr[2]).orderBy("accounts.id", "ASC").skip(skip).getMany();
+                break;
+
+                case 4:
+                  faccount = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).andWhere(datafr[2]).andWhere(datafr[3]).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+                  tot = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).andWhere(datafr[2]).andWhere(datafr[3]).orderBy("accounts.id", "ASC").skip(skip).getMany();
+                break;
+
+                case 5:
+                  faccount = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).andWhere(datafr[2]).andWhere(datafr[3]).andWhere(datafr[4]).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+                  tot = yield accountRepository.createQueryBuilder("accounts").select(datafl).where(datafr[0]).andWhere(datafr[1]).andWhere(datafr[2]).andWhere(datafr[3]).andWhere(datafr[4]).orderBy("accounts.id", "ASC").skip(skip).getMany();
+                break;
+
+                default:
+                  faccount = yield accountRepository.createQueryBuilder("accounts").select(datafl).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+                  tot = yield accountRepository.createQueryBuilder("accounts").select(datafl).orderBy("accounts.id", "ASC").skip(skip).getMany();
+                break;
+              }
+            } else {
+                faccount = yield accountRepository.createQueryBuilder("accounts").select(datafl).orderBy("accounts.id", "ASC").skip(skip).take(size).getMany();
+                tot = yield accountRepository.createQueryBuilder("accounts").select(datafl).orderBy("accounts.id", "ASC").skip(skip).getMany();
+            }
+
+            // Load accounts by body specified parameter
+            const account = faccount;
+            var acctotal1 = Object.keys(tot).length;
+            var qptotal = Math.ceil(acctotal1/size);
             // if account was not found return 404 to the client
             if (!account) {
                 response.status(404);
@@ -71,7 +127,7 @@ function accountGetByPageAction(request, response) {
                 return;
             }
             // return loaded account and total page number
-            response.send({account:account, pagetotal:pagetotal});
+            response.send({"Accounts":account, "Selected Accounts Total":acctotal1, "Selected Pages Total":qptotal, "Accounts Total":acctotal, "Pages Available Total":pagetotal});
         });
   }
      } else {
