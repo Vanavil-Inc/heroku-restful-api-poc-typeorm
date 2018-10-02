cf34849e08a8627c8fa3c8c5ab50569a2220aee8
Harxen <harxen.k@gmail.com>
2018-10-02 10:32:21 -0400

    Added the network retry to AccountGetByPageAction.js

--- src/controller/AccountGetByPageAction.js
+++ src/controller/AccountGetByPageAction.js
@@ -64,107 +64,147 @@ function accountGetByPageAction(request, response) {
         })
 
         // get a account repository to perform operations with account
-        // try {
-        //   const accountRepository = typeorm_1.getManager().getRepository(Account_1.Accounts);
-        // }
-        // catch (er) {
-        //   if (er === "ConnectionNotFoundError") {
-        //       console.log("Test Successful!");
-        //   }
-        //   if (er === "RpositoryNotFoundError") {
-        //       console.log("Test Successful!");
-        //   }
-        // }
         const accountRepository = typeorm_1.getManager().getRepository(Account_1.Account);
-        let repCon = accountRepository.manager.connection.isConnected;
-        console.log(repCon);
-        if (repCon !== true) {
-          for (let j = 0; j < 4; j++) {
-            setTimeout(async function() {
-              console.log("========================================================");
-              console.log("Attempt #" + j + " to Reconnect...");
-              await accountRepository.manager.connection.connect();
-              console.log("========================================================");
-            }, 3000);
-            if (repCon === true) {
-              break;
-            } else if ((j === 3) && (repCon === false)) {
-              response.json({
-                message: "Cannot Connect to the Database"
-              })
-              response.end();
-            }
-          }
-        }
+
+        // let repCon = accountRepository.manager.connection.isConnected;
+        // console.log("========================================================");
+        // console.log(repCon);
+        // console.log("========================================================");
+        // if (repCon !== true) {
+        //   for (let j = 0; j < 4; j++) {
+        //     setTimeout(async function() {
+        //       console.log("========================================================");
+        //       console.log("Attempt #" + j + " to Reconnect...");
+        //       await accountRepository.manager.connection.connect().catch(error => console.log("Error: ", error));
+        //       console.log("========================================================");
+        //     }, 3000);
+        //     if (repCon === true) {
+        //       break;
+        //     } else if ((j === 3) && (repCon === false)) {
+        //       response.json({
+        //         message: "Cannot Connect to the Database"
+        //       })
+        //       response.end();
+        //     }
+        //   }
+        // }
 
         // getting account list for help in pagination
-        const acclist = yield accountRepository.find();
-        const qb = accountRepository.createQueryBuilder("accounts");
-        var acctotal = Object.keys(acclist).length;
-        // pulling page size information
-        size = parseInt(request.body.size);
-        // determines by how many items to skip based on page selection
-        pagenumber = parseInt(request.body.page);
-        pagetotal = Math.ceil(acctotal/size);
-        // logic for page number range
-        if ((pagenumber <= pagetotal) && (pagenumber >= 1)) {
-          skip = (pagenumber - 1) * size;
-        } else {
-          response.json({
-            message: "Requested Page is Unavailable"
-          })
-          response.end();
-          return;
-        }
-        // fieldset criteria
-        fieldlist = String(request.body.fieldlist).split(" ");
-        var datafl = [];
-        for (var i = 0; i < fieldlist.length; i++) {
-          datafl.push(fieldlist[i]);
-        }
-        // Forming the QueryBuilder
-        qb.select(datafl)
-        // Adding the filter criteria
-        filter = request.body.filter;
-        if (filter) {
-          if (filter.condition.length >= 2) {
-            qb.where(filter.wherefilter[0]);
-            for (var i = 1; i < filter.wherefilter.length; i++) {
-              if (filter.condition[i] === "AND") {
-                qb.andWhere(filter.wherefilter[i]);
-              } else{
-                qb.orWhere(filter.wherefilter[i]);
+        // const acclist = yield accountRepository.find();
+
+
+        //variables for retry logic
+        var maxNoOfRetries = 3; // maximum 3 retries
+        var timeDelayBetweenRetry = 20000; //20 seconds
+        var currentRetryCount = 0;
+        var currentStatus = "";
+
+        do {
+          try {
+            console.log("CurrentRetryCount= ", currentRetryCount);
+            // getting account list for help in pagination
+            const acclist = yield accountRepository.find();
+            // if account was not found return 404 to the client
+            currentStatus = "success";
+            if (acclist) {
+              console.log("Account Lookup Success");
+              // return loaded accounts
+              const qb = accountRepository.createQueryBuilder("accounts");
+              var acctotal = Object.keys(acclist).length;
+              // pulling page size information
+              size = parseInt(request.body.size);
+              // determines by how many items to skip based on page selection
+              pagenumber = parseInt(request.body.page);
+              pagetotal = Math.ceil(acctotal/size);
+              // logic for page number range
+              if ((pagenumber <= pagetotal) && (pagenumber >= 1)) {
+                skip = (pagenumber - 1) * size;
+              } else {
+                response.json({
+                  message: "Requested Page is Unavailable"
+                })
+                response.end();
+                return;
               }
+              // fieldset criteria
+              fieldlist = String(request.body.fieldlist).split(" ");
+              var datafl = [];
+              for (var i = 0; i < fieldlist.length; i++) {
+                datafl.push(fieldlist[i]);
+              }
+              // Forming the QueryBuilder
+              qb.select(datafl)
+              // Adding the filter criteria
+              filter = request.body.filter;
+              if (filter) {
+                if (filter.condition.length >= 2) {
+                  qb.where(filter.wherefilter[0]);
+                  for (var i = 1; i < filter.wherefilter.length; i++) {
+                    if (filter.condition[i] === "AND") {
+                      qb.andWhere(filter.wherefilter[i]);
+                    } else{
+                      qb.orWhere(filter.wherefilter[i]);
+                    }
+                  }
+                }
+              }
+              // Second logic for page number range
+              var acctotal1 = yield qb.getCount();
+              var qptotal = Math.ceil(acctotal1/size);
+              if ((pagenumber <= qptotal) && (pagenumber >= 1)) {
+                skip = (pagenumber - 1) * size;
+              } else {
+                response.send("Requested Page is Unavailable");
+                response.end();
+                return;
+              }
+              // Finally putting it all together
+              qb.orderBy("accounts.id", "ASC").skip(skip).take(size);
+              // Load accounts by body specified parameter
+              const account = yield qb.getMany();
+              // if account was not found return 404 to the client
+              if (!account) {
+                response.status(404);
+                response.end();
+                return;
+              }
+              // return loaded account and total page number
+              response.send({"Accounts":account, "Selected Accounts Total":acctotal1, "Selected Pages Total":qptotal, "Accounts Total":acctotal, "Pages Available Total":pagetotal});
+
+            } else {
+              console.log("No Records Found");
+              response.status(404).send("No Records Found");
+            }
+          } catch(err) {
+            console.log("Error is caught inside catch: ", err);
+            if (err.code == 'ECONNREFUSED') {
+              currentRetryCount += 1;
+              console.log( "changed currentRetryCount to: ", currentRetryCount);
+              if (currentRetryCount > maxNoOfRetries) {
+                currentStatus = "failed";
+                response.status(500).send(err);
+              } else {
+                console.log("Before the timer");
+                freeze(timeDelayBetweenRetry);
+                console.log("After the timer");
+              }
+            } else {
+              currentStatus = "failed";
+              response.status(500).send(err);
             }
           }
-        }
-        // Second logic for page number range
-        var acctotal1 = yield qb.getCount();
-        var qptotal = Math.ceil(acctotal1/size);
-        if ((pagenumber <= qptotal) && (pagenumber >= 1)) {
-          skip = (pagenumber - 1) * size;
-        } else {
-          response.send("Requested Page is Unavailable");
-          response.end();
-          return;
-        }
-        // Finally putting it all together
-        qb.orderBy("accounts.id", "ASC").skip(skip).take(size);
-        // Load accounts by body specified parameter
-        const account = yield qb.getMany();
-        // if account was not found return 404 to the client
-        if (!account) {
-          response.status(404);
-          response.end();
-          return;
-        }
-        // return loaded account and total page number
-        response.send({"Accounts":account, "Selected Accounts Total":acctotal1, "Selected Pages Total":qptotal, "Accounts Total":acctotal, "Pages Available Total":pagetotal});
+        } while(currentRetryCount <= maxNoOfRetries && currentStatus == "");
       });
     }
   } else {
     response.sendStatus(403);
   }
 }
+
+function freeze(time) {
+  const stop = new Date().getTime() + time;
+  while(new Date().getTime() < stop);
+}
+
 exports.accountGetByPageAction = accountGetByPageAction;
 //# sourceMappingURL=AccountGetByPageAction.js.map
